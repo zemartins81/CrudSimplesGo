f2f3aa8675a72bbd733dfbf52b10db55b8401b91
José Carlos Martins <zemartins81@gmail.com>
2023-03-21 18:13:17 -0300

    reestruturação da api

--- databases/database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `users` (
-    `id` int(6) NOT NULL UNIQUE AUTO_INCREMENT,
-    `name` varchar(50) NOT NULL,
-    `email` varchar(50) NOT NULL UNIQUE,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
--- entities/user.go
+++
@@ -1,7 +0,0 @@
-package entities
-
-type User struct {
-	Id    int    `json:"id,omitempty"`
-	Name  string `json:"name,omitempty"`
-	Email string `json:"email,omitempty"`
-}
